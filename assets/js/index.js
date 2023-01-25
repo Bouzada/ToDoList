3d5cce136cbd3c1901af21006951f3574819783b
Hugo Bouzada <hugobouzada@gmail.com>
2023-01-25 17:16:58 -0300

    Adicionando função para gerar número aleatório.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,20 @@
 const listTasks = document.getElementById('listTasks')
 
+const min = 0;
+const max = 51
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 document.getElementById('btnAddTask').addEventListener('click', function() {
   const input = document.getElementById('addNewTask').value
 
   const task = document.createElement('li')
   task.classList = "task"
+  task.id = getRandomInt(min, max)
   task.innerHTML = `
     <span class="taskText">${input}</span>
     <div class="">
@@ -19,6 +29,6 @@ document.getElementById('btnAddTask').addEventListener('click', function() {
 
   listTasks.appendChild(task)
 
-  input.value = ''
+  document.getElementById('addNewTask').value = ''
 })
 
