a99666a38f3ff3d1c6dda8df43e01cb4f387e0b7
Hugo Bouzada <hugobouzada@gmail.com>
2023-01-26 17:44:54 -0300

    Alterando o limite do número ID das tasks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,8 @@
 const listTasks = document.getElementById('listTasks')
+const ids = []
 
 const min = 0;
-const max = 51
+const max = 200
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -11,17 +12,19 @@ function getRandomInt(min, max) {
 
 document.getElementById('btnAddTask').addEventListener('click', function() {
   const input = document.getElementById('addNewTask').value
+  const numberID = getRandomInt(min, max)
 
   const task = document.createElement('li')
   task.classList = "task"
-  task.id = "task-" + getRandomInt(min, max)
+  task.id = "task-" + numberID
+
   task.innerHTML = `
     <span class="taskText">${input}</span>
     <div class="">
-      <button class="btn btnAction btnRemove" id="btnRemove"> 
+      <button class="btn btnAction btnRemove" id="btnRemove" value="task-${numberID}"> 
         <i class="fa-solid fa-trash"></i>
       </button>
-      <button class="btn btnAction btnDo" id="btnDo">
+      <button class="btn btnAction btnDo" id="btnDo" value="task-${numberID}">
         <i class="fa-solid fa-check"></i>
       </button>
     </div>
@@ -30,5 +33,7 @@ document.getElementById('btnAddTask').addEventListener('click', function() {
   listTasks.appendChild(task)
 
   document.getElementById('addNewTask').value = ''
-})
 
+  ids.unshift(task.id)
+  console.log(ids)
+})
